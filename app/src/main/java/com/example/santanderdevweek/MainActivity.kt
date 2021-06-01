0cd36be9ff2a186adae1107540783cecfd5a6406
Parizloco <parizottoloco@hotmail.com>
2021-06-01 19:05:18 -0300

    videoaula_1:14

--- app/src/main/java/com/example/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d("Click","Clique")
+              true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
+
 }
